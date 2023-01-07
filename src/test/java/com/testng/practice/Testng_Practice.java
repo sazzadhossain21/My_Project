b07f4f86d45030425d9ba2da4de1cc2ec954073c
sazzadhossain21 <compus1983@gmail.com>
2023-01-07 01:46:50 -0500

    this is my rebase commit

--- src/test/java/com/testng/practice/Testng_Practice.java
+++ src/test/java/com/testng/practice/Testng_Practice.java
@@ -2,6 +2,6 @@ package com.testng.practice;
 
 public class Testng_Practice {
 public static void main(String[] args) {
-	System.out.println("This is my practice of git");
+	System.out.println("this is second testing");
 }
 }
