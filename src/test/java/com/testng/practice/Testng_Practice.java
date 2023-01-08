e635bd27cd5a5e94cfdf0e69731c4385b903c0bf
sazzadhossain21 <compus1983@gmail.com>
2023-01-08 15:43:07 -0500

    commit 3.42

--- src/test/java/com/testng/practice/Testng_Practice.java
+++ src/test/java/com/testng/practice/Testng_Practice.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("this is second testing");
 }
 public void test() {
-	System.out.println("Hi");
+	System.out.println("commit 3.42");
 }
 
 
